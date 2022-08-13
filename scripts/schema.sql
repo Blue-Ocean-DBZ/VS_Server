e081227c33b0b880f64fda34931a65c7e191035a
Shannon Murphy <Murphy1693@gmail.com>
2022-08-13 13:46:26 -0700

    Postgres Schema added

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,13 +1,11 @@
-CREATE DATABASE vegetationstation;
-
-USE vegetationstation;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR NOT NULL,
+  session_id VARCHAR NOT NULL,
   profile_pic VARCHAR DEFAULT NULL,
   longitude DECIMAL NOT NULL,
-  latitude DECIMAL NOT NULL,
+  latitude DECIMAL NOT NULL
 );
 
 CREATE TABLE plants (
@@ -15,9 +13,9 @@ CREATE TABLE plants (
   plant_name VARCHAR NOT NULL,
   photo VARCHAR NOT NULL,
   deleted BOOLEAN DEFAULT false,
-  user_id INT,
+  owner_id INT,
   CONSTRAINT fk_owner
-  FOREIGN KEY(user_id)
+  FOREIGN KEY(owner_id)
   REFERENCES "users"(id)
 );
 
@@ -26,22 +24,42 @@ CREATE TABLE favorites (
   user_id INT,
   CONSTRAINT fk_user
   FOREIGN KEY(user_id)
-  REFERENCES "users"(id)
+  REFERENCES "users"(id),
   plant_id INT,
   CONSTRAINT fk_plant_id
   FOREIGN KEY(plant_id)
   REFERENCES "plants"(id)
 );
 
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  user_id_one INT,
+  CONSTRAINT fk_user_one
+  FOREIGN KEY(user_id_one)
+  REFERENCES "users"(id),
+  user_id_two INT,
+  CONSTRAINT fk_user_two
+  FOREIGN KEY(user_id_two)
+  REFERENCES "users"(id),
+  content VARCHAR NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE trades (
   id SERIAL PRIMARY KEY,
   pending BOOLEAN DEFAULT true,
+  accepted BOOLEAN DEFAULT NULL,
   user_offer_id INT,
-  CONSTRAINT fk_offer
+  CONSTRAINT fk_user_offer
   FOREIGN KEY(user_offer_id)
   REFERENCES "users"(id),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  message_id INT,
+  CONSTRAINT fk_message
+  FOREIGN KEY(message_id)
+  REFERENCES "messages"(id),
   user_target_id INT,
-  CONSTRAINT fk_offer
+  CONSTRAINT fk_user_target
   FOREIGN KEY(user_target_id)
   REFERENCES "users"(id)
 );
@@ -51,11 +69,11 @@ CREATE TABLE trade_components (
   trade_id INT,
   CONSTRAINT fk_trade_id
   FOREIGN KEY(trade_id)
-  REFERENCES "trades"(id)
+  REFERENCES "trades"(id),
   plant_id INT,
   CONSTRAINT fk_plant_id
   FOREIGN KEY(plant_id)
-  REFERENCES "plants"(id)
+  REFERENCES "plants"(id),
   user_id INT,
   CONSTRAINT fk_user_id
   FOREIGN KEY(user_id)
