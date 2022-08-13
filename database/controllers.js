4a219c5ed01cd25b5a35ca7423867c8649f0c1fb
Shannon Murphy <Murphy1693@gmail.com>
2022-08-12 19:57:58 -0700

    Schema changes

--- database/controllers.js
+++ database/controllers.js
@@ -6,5 +6,27 @@ module.exports = {
   addUser: function (userObject) {
     return save(userObject);
   },
+
+  editUser: function (userId) {},
+
+  addPlant: function (userId, plantObject) {},
+
+  addToFavorites: function (userId, plantId) {},
+
+  removeFromFavorites: function (userId, plantId) {},
+
+  requestTrade: function (userId, targetUserId, plantsOffer, plantsRequest) {},
+
+  handleTrade: function (tradeId, acceptOrReject) {},
+
+  removePlant: function (plantId) {},
 };
+// addPlant
 // findByLocation
+// addToFavorites
+// requestTrade
+// handleTrade
+// addUser
+// editUser
+// removePlant
+// removeFromFavorites
--- database/models.js
+++ database/models.js
@@ -2,33 +2,52 @@ const mongoose = require("mongoose");
 
 // location -> [longitude, latitude]
 
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+});
+
 const plantSchema = new mongoose.Schema({
   name: String,
   category: [{ type: String }],
   photo: String,
+  owner: String,
 });
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  profile_picture: String,
   username: String,
-  password: String,
+  profile_picture: String,
   plants: [
     {
       type: plantSchema,
     },
   ],
+
   location: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
-    },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
+    type: pointSchema,
+    required: true,
   },
+
+  favorites: [
+    {
+      type: plantSchema,
+    },
+  ],
+});
+
+const tradeSchema = new mongoose.Schema({
+  userId: String,
+  targetUserId: String,
+  userPlantsOffer: [{ type: plantSchema }],
+  targetPlant: [{ type: plantSchema }],
+  pending: Boolean,
 });
 
 const User = mongoose.model("User", userSchema);
