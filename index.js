592731479f54a7fe94216449ecfa2dc05a959641
Shannon Murphy <Murphy1693@gmail.com>
2022-08-13 15:58:09 -0700

    Postgres Schema (#2)
    * VS Server Initialization
    
    * gitkeep
    
    * Prehook implemented
    
    * Linter added
    
    * eslint profile added
    
    * Schema changes
    
    * Controllers changes / geolocation (mongo)
    
    * schema added for postgres
    
    * Postgres Schema added
    
    * Repurposed for psql
    
    * prehook commit enabled

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const db = require("./database");
 const express = require("express");
-const addUser = require("./database/controllers.js").addUser;
+const controllers = require("./database/controllers.js");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("GET");
-  res.send();
-});
+app.get("/all", controllers.findByLocation);
 
-app.post("/user", (req, res) => {
-  console.log("POST:", req.body);
-  addUser(req.body).then(() => {
-    res.status(201).send("success!");
-  });
-});
+app.post("/plant", controllers.addPlant);
+
+app.delete("/plant/:id", controllers.removePlant);
+
 
 app.listen(process.env.PORT);
 console.log(`listening on port ${process.env.PORT}`);
---
+++ scripts/initializeDB.js
@@ -0,0 +1,115 @@
+const pg = require("pg");
+require("dotenv").config();
+
+let client = new pg.Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+const users_table = `CREATE TABLE users (\
+  id SERIAL PRIMARY KEY, \
+  username VARCHAR NOT NULL, \
+  session_id VARCHAR NOT NULL, \
+  profile_pic VARCHAR DEFAULT NULL, \
+  longitude DECIMAL NOT NULL, \
+  latitude DECIMAL NOT NULL \
+);`;
+
+const plants_table = `CREATE TABLE plants (\
+  id SERIAL PRIMARY KEY, \
+  plant_name VARCHAR NOT NULL, \
+  photo VARCHAR NOT NULL, \
+  deleted BOOLEAN DEFAULT false, \
+  owner_id INT, \
+  CONSTRAINT fk_owner \
+  FOREIGN KEY(owner_id) \
+  REFERENCES "users"(id) \
+);`;
+
+const favorites_table = `CREATE TABLE favorites (\
+  id SERIAL PRIMARY KEY, \
+  user_id INT, \
+  CONSTRAINT fk_user \
+  FOREIGN KEY(user_id) \
+  REFERENCES "users"(id), \
+  plant_id INT, \
+  CONSTRAINT fk_plant_id \
+  FOREIGN KEY(plant_id) \
+  REFERENCES "plants"(id) \
+);`;
+
+const messages_table = `CREATE TABLE messages ( \
+  id SERIAL PRIMARY KEY, \
+  user_id_one INT, \
+  CONSTRAINT fk_user_one \
+  FOREIGN KEY(user_id_one) \
+  REFERENCES "users"(id), \
+  user_id_two INT, \
+  CONSTRAINT fk_user_two \
+  FOREIGN KEY(user_id_two) \
+  REFERENCES "users"(id), \
+  content VARCHAR NOT NULL, \
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP \
+);`;
+
+const trades_table = `CREATE TABLE trades ( \
+  id SERIAL PRIMARY KEY, \
+  pending BOOLEAN DEFAULT true, \
+  accepted BOOLEAN DEFAULT NULL, \
+  user_offer_id INT, \
+  CONSTRAINT fk_user_offer \
+  FOREIGN KEY(user_offer_id) \
+  REFERENCES "users"(id), \
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+  message_id INT, \
+  CONSTRAINT fk_message \
+  FOREIGN KEY(message_id) \
+  REFERENCES "messages"(id), \
+  user_target_id INT, \
+  CONSTRAINT fk_user_target \
+  FOREIGN KEY(user_target_id) \
+  REFERENCES "users"(id) \
+);`;
+
+const trade_components_table = `CREATE TABLE trade_components ( \
+  id SERIAL PRIMARY KEY, \
+  trade_id INT, \
+  CONSTRAINT fk_trade_id \
+  FOREIGN KEY(trade_id) \
+  REFERENCES "trades"(id), \
+  plant_id INT, \
+  CONSTRAINT fk_plant_id \
+  FOREIGN KEY(plant_id) \
+  REFERENCES "plants"(id), \
+  user_id INT, \
+  CONSTRAINT fk_user_id \
+  FOREIGN KEY(user_id) \
+  REFERENCES "plants"(id) \
+);`;
+
+client
+  .connect()
+  .then(() => {
+    return client.query(users_table);
+  })
+  .then(() => {
+    return client.query(plants_table);
+  })
+  .then(() => {
+    return client.query(favorites_table);
+  })
+  .then(() => {
+    return client.query(messages_table);
+  })
+  .then(() => {
+    return client.query(trades_table);
+  })
+  .then(() => {
+    return client.query(trade_components_table);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
