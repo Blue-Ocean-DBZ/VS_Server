e03839bb74d493c665fce937dcb777850fe0b7c7
Shannon Murphy <Murphy1693@gmail.com>
2022-08-18 20:20:02 -0700

    notifications added

--- database/models.js
+++ database/models.js
@@ -135,6 +135,7 @@ module.exports = {
       t.id trade_id,
       t.pending,
       t.accepted,
+      t.notifications1 + t.notifications2 notifications,
       t.shown_to_user_offer,
       t.shown_to_user_target,
       t.created_at,
@@ -150,7 +151,9 @@ module.exports = {
     FROM
       (
         SELECT
-          *
+          *,
+          (SELECT COUNT(*) FROM trades t WHERE t.user_target_id = $1 AND t.shown_to_user_target = false) notifications1,
+          (SELECT COUNT(*) FROM trades t WHERE t.user_offer_id = $1 AND t.shown_to_user_offer  = false) notifications2
         FROM
           trades t
         WHERE
