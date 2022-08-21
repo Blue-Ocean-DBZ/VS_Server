c36fbd39edf562fb65a6cbf86c7543f4286df781
Shannon Murphy <Murphy1693@gmail.com>
2022-08-20 20:21:01 -0700

    Server ready (#23)
    * Changes to queries
    
    * myPlants endpoint added
    
    * Changed query
    
    * Changed to firebase_id
    
    * Favorites Schema added distance
    
    * Working addToFavorites distance query
    
    * getFavorites has distance
    
    * more server work, editUser put request endpoint added
    
    * Some message functionality
    
    * various changes
    
    * FB queries added
    
    * FB Queries added
    
    * Changes to firebase_id methods
    
    * created_at timestamp changed in trades on accept/decline
    
    * Cleared comments
    
    * Server zip-city-count-state
    
    * /tmp/
    
    * editQuery added new zip info
    
    * City info added to geoquery
    
    * Default true for requestTrade shown_to_offer
    
    * Changes for server;
    
    * notifications added
    
    * Updated README.md, reconfigured queries, added geolocation update script
    
    * Updated README
    
    * changed README
    
    * Cleaning up repo
    
    * Further cleaning
    
    * Changes to queries
    
    * myPlants endpoint added
    
    * more server work, editUser put request endpoint added
    
    * Some message functionality
    
    * FB queries added
    
    * FB Queries added
    
    * Server zip-city-count-state
    
    * /tmp/
    
    * City info added to geoquery
    
    * Changes for server;
    
    * notifications added
    
    * Updated README.md, reconfigured queries, added geolocation update script
    
    * Cleaning up repo
    
    * Further cleaning
    
    * Removed duplicate;

--- database/models.js
+++ database/models.js
@@ -115,7 +115,6 @@ module.exports = {
       (SELECT COUNT(*) FROM trades t WHERE (t.user_target_id = $1 AND t.shown_to_user_target = false) OR (t.user_offer_id = $1 AND t.shown_to_user_offer = false)) notifications,
       t.pending,
       t.accepted,
-      t.notifications1 + t.notifications2 notifications,
       t.shown_to_user_offer,
       t.shown_to_user_target,
       t.created_at,
@@ -136,9 +135,7 @@ module.exports = {
     FROM
       (
         SELECT
-          *,
-          (SELECT COUNT(*) FROM trades t WHERE t.user_target_id = $1 AND t.shown_to_user_target = false) notifications1,
-          (SELECT COUNT(*) FROM trades t WHERE t.user_offer_id = $1 AND t.shown_to_user_offer  = false) notifications2
+          *
         FROM
           trades t
         WHERE
